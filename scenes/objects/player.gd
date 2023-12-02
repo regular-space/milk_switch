288a7d4efcc10aff2c884ca680ac6093fc18492b
regular space <kevin.sugay@gmail.com>
2023-12-01 21:30:38 -0500

    Update player.gd

--- scenes/objects/player.gd
+++ scenes/objects/player.gd
@@ -1,20 +1,20 @@
 # Top Down 2D
 extends CharacterBody2D
 
-@export var speed: float = 100.0
-@export var deceleration: float = 50.0
+@export var speed: int = 100
+@export var deceleration: int = 50
 
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 	if direction:
 		# Animation
-		if direction == Vector2.UP:
+		if direction.y < 0:
 			$AnimatedSprite2D.play("up")
-		elif direction == Vector2.DOWN:
+		elif direction.y > 0:
 			$AnimatedSprite2D.play("down")
-		elif direction == Vector2.RIGHT:
+		elif direction.x > 0:
 			$AnimatedSprite2D.play("right")
-		elif direction == Vector2.LEFT:
+		elif direction.x < 0:
 			$AnimatedSprite2D.play("left")
 		
 		velocity = direction * speed
