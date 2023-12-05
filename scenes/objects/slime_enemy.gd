04d9f24d5c08ffc3ec0b946043373f455035841e
Nahte101 <42877810+Nahte101@users.noreply.github.com>
2023-12-06 06:51:14 +1100

    Enemy object avoidance achieved
    - It's not perfect but it's working so that's a step in the right direction
    - Still need to clean it up a bunch

---
+++ scenes/objects/slime_enemy.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+@export var mov_speed := 50.0
+
+@export var movement_target: Node2D
+@export var nav_agent: NavigationAgent2D
+
+func _ready():
+	nav_agent.path_desired_distance = 4.0
+	nav_agent.target_desired_distance = 4.0
+	
+	call_deferred('actor_setup')
+	
+func actor_setup():
+	await get_tree().physics_frame
+	set_move_target(movement_target.position)
+	
+func set_move_target(target: Vector2):
+	nav_agent.target_position = target
+	
+func _physics_process(delta):
+	if nav_agent.is_navigation_finished():
+		return
+	var current_agent_pos: Vector2 = global_position
+	var next_path_position: Vector2 = nav_agent.get_next_path_position()
+	var steering := (next_path_position - current_agent_pos)
+	velocity += steering
+	nav_agent.set_velocity(velocity)
+
+
+func _on_velocity_computed(safe_velocity):
+	velocity = safe_velocity
+	move_and_slide()
