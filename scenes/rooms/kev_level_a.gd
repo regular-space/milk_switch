f25abba873cb34e8713fb654016a85cdc96210fc
regular space <kevin.sugay@gmail.com>
2023-12-15 17:00:56 -0500

    Fixed position of level c

--- scenes/rooms/kev_level_a.gd
+++ scenes/rooms/kev_level_a.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+var added_test_vfx = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ready.connect(Global._on_ready_room.bind(self))
 	#print(name)
 
+func _physics_process(delta):
+	if not added_test_vfx:
+		Vfx.play_vfx(Vector2(12, 12), "blood_splurt")
+		added_test_vfx = true
 
 
 func _on_stairs_body_entered(body):
